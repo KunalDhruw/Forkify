97744b8b63ce4560949e2d3bee4d1f766e8a7a1b
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-03-07 13:58:06 +0530

    First Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '832e0464-9700-4eb2-aeee-8d23aae6ccba';
+export const MODAL_CLOSE_SEC = 2.5;
